a8d5eafd858dc930c837270318970d26f2ff472a
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2024-11-15 07:58:30 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://hashgo-api.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+};
